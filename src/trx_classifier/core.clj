66d7f28ac933a22a4d5435a795e093d5a4727000
Jose Aviles <javiles@Joses-MBP-2.tampabay.rr.com>
2024-06-01 12:23:14 -0400

    change args parse

--- src/trx_classifier/core.clj
+++ src/trx_classifier/core.clj
@@ -5,15 +5,15 @@
 
 (defn parse-args [args]
   (if (seq args)
-    (doseq [arg args]
-      (println arg))
+    (let [[[dir & files]] (into [] args)]
+      [dir files])
     (println "No args provided")))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (let [[file config-dir] (parse-args args)]
-    (io/read-csv file config-dir ".")))
+  (let [[config-dir files] (parse-args args)]
+    (io/read-csv files config-dir ".")))
 
 
 
